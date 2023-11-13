146be571699d7160715595287c9eed567da84db0
Emiliano <emilianodebenedetti@outlook.com>
2023-11-13 16:31:37 -0300

    Fixeo bug PrivateRoutes

--- crud-stock/src/App.js
+++ crud-stock/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import { HashRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, HashRouter, Navigate } from 'react-router-dom';
 import Create from './components/Create';
 import Edit from './components/Edit';
 import Show from './components/Show';
 import Navbar from './components/Navbar';
 import Pie from './components/Pie';
 import Login from './components/Login';
+import React, { useEffect, useState } from 'react';
+import { auth } from './firebaseConfig/firebase';
 
+const PrivateRoute = ({ element }) => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return authenticated ? <React.Fragment>{element}</React.Fragment> : <Navigate to="/" />;
+};
 
 function App() {
   return (
@@ -15,9 +34,9 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={ <Login />} />
-          <Route path='/home' element={ <Show />} />
-          <Route path='/create' element={ <Create />} />
-          <Route path='/edit/:id' element={ <Edit />} />
+          <Route path="/home" element={<PrivateRoute element={<Show />} />} />
+          <Route path="/create" element={<PrivateRoute element={<Create />} />} />
+          <Route path="/edit/:id" element={<PrivateRoute element={<Edit />} />} />
         </Routes>
       </Router>
       <Pie />
